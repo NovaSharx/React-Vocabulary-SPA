cef5ef2dbfd2399370e4bfdbb89ca6fa6f03e59c
Davin <davin-dj@hotmail.com>
2022-12-07 22:08:48 -0500

    Installed react packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import * as Mui from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mui.Tabs centered>
+  <Mui.Tab label="Item One" />
+  <Mui.Tab label="Item Two" />
+  <Mui.Tab label="Item Three" />
+</Mui.Tabs>
     </div>
-  );
+  )
 }
 
 export default App;
